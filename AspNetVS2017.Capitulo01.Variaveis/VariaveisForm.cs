64a724e4da1c8d2b64edc007878bd65fb9995fb4
Wanderley Silva <wanderley_dsilva@yahoo.com.br>
2018-08-10 19:37:17 -0300

    DesignTroco

---
+++ AspNetVS2017.Capitulo01.Variaveis/VariaveisForm.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace AspNetVS2017.Capitulo01.Variaveis
+{
+    public partial class VariaveisForm : Form
+    {
+        //abaixo o campos ou atributos (fields) da classe
+       private int _x = 32;
+       private int _w = 45;
+       private int _y = 16;
+       private int _z = 32;
+
+
+        public VariaveisForm()
+        {
+            InitializeComponent();
+        }
+
+        private void resultadoListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void operaçõesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+
+
+
+
+
+
+            //long quantidadeHabitantes = 7000000000;
+            //string nome = "Wanderley";
+
+            //decimal valor = 19.57m;
+            ////var pode ser utilizado ao inves do nome da variavel.
+            //bool aprovado; // pode ser utilizado var aprovado = true;
+            ////@class
+
+            //DateTime nascimento = new DateTime(1987, 09, 04);
+            //var agora = DateTime.Now;
+
+        }
+        private void aritmeticasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            /*variaveis*/
+            // ctrl + k + c transforma parte do codigo em comentario.
+
+            int a = 2;
+            int b = 6, c = 10, d = 13;
+
+            resultadoListBox.Items.Add("a = " + a);
+            resultadoListBox.Items.Add(string.Concat("b = ", b)); //concatena
+            resultadoListBox.Items.Add(string.Format("c = {0}", c)); //string format para o numero dentro da chave é referente ao indice e : pode formatar
+            resultadoListBox.Items.Add($"d = " + d); //$ string interpolado, bem parecido com format, porem dentro da chave voce usa a propria variavel.
+            resultadoListBox.Items.Add("---------------------");
+            resultadoListBox.Items.Add("c * d = " + (c * d));
+            resultadoListBox.Items.Add("c / d = " + (c / d));
+            resultadoListBox.Items.Add("d % a = " + (d % a));
+
+
+
+        }
+
+
+        private void reduzidasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var x = 5;
+            // x = x - 3;
+            //x -= 3;
+
+            resultadoListBox.Items.Add("---------------");
+            resultadoListBox.Items.Add("x = " + x);
+            resultadoListBox.Items.Add("x -= 3 = " + (x -= 3));
+
+        }
+
+        private void incrementaisDecrementaisToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            int a;
+
+            a = 5;
+            resultadoListBox.Items.Add(new string('-', 50));
+            resultadoListBox.Items.Add("Exemplo de Pré Incremental" );
+            resultadoListBox.Items.Add("a = " + a);
+            resultadoListBox.Items.Add("2 + ++a = " + (2+ ++a));
+            resultadoListBox.Items.Add("a = " + a);
+
+            resultadoListBox.Items.Add(new string('-', 50));
+
+            a = 5;
+            resultadoListBox.Items.Add(new string('-', 50));
+            resultadoListBox.Items.Add("Exemplo de Pós Incremental");
+            resultadoListBox.Items.Add("a = " + a);
+            resultadoListBox.Items.Add("2 + a++ = " + (2 + a++));
+            resultadoListBox.Items.Add("a = " + a);
+        }
+
+        private void BooleanasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ExibirValoresVariaveis();
+
+            resultadoListBox.Items.Add(new string('-', 50));
+
+            resultadoListBox.Items.Add("w <= x = " + (_w <= _x)); //
+            resultadoListBox.Items.Add("x == z = " + (_x == _z)); //
+            resultadoListBox.Items.Add("x != z = " + (_x != _z)); //
+
+        }
+
+        private void ExibirValoresVariaveis() // assinatura do metodo // void quando nao tem nada
+        {
+            resultadoListBox.Items.Add(new string('-', 50));
+            resultadoListBox.Items.Add("x = " + _x);
+            resultadoListBox.Items.Add("w = " + _w);
+            resultadoListBox.Items.Add("y = " + _y);
+            resultadoListBox.Items.Add("z = " + _z);
+        }
+
+        private void logicasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ExibirValoresVariaveis();
+
+            resultadoListBox.Items.Add("_w <= x || _y == 16 = " + (_w <= _x || _y ==16 )); //
+            resultadoListBox.Items.Add("_x == _z && _z!= _x = " + (_x == _z && _z != _x)); //
+            resultadoListBox.Items.Add("!(_y > _w) = " + (!(_y > _w))); //
+
+            
+        }
+
+        private void ternariasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //operador ternario "?" - sempre booleano true ou false
+            //if ou else
+
+            int ano;
+
+            ano = 2018;
+            resultadoListBox.Items.Add(string.Format("O ano {0} é bissexto? {1}." , ano,
+                ano % 4 == 0 ? "Sim" : "Não")); // operador ternario % == 0  ?
+
+            ano = 2020;
+            resultadoListBox.Items.Add($"O ano {ano} é bissexto? {(ano % 4 == 0 ? "Sim" : "Não")}.");
+            resultadoListBox.Items.Add($"O ano {ano} é bissexto? {(DateTime.IsLeapYear(ano) ? "Sim" : "Não")}.");
+
+
+
+            // var resposta = "";
+            // if (ano % 4 == 0)
+            // {
+            //     resposta = "Sim";         
+            // }
+            // else
+            // {
+            //     resposta = "Não";
+            // }
+
+
+
+        }
+    }
+}
---
+++ AspNetVS2017.cap01.Troco/TrocoForm.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace AspNetVS2017.cap01.Troco
+{
+    partial class TrocoForm
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TrocoForm));
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem(new string[] {
+            ""}, 5, System.Drawing.Color.Empty, System.Drawing.Color.Empty, new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0))));
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem(new string[] {
+            ""}, 4, System.Drawing.Color.Empty, System.Drawing.Color.Empty, new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0))));
+            System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem(new string[] {
+            ""}, 3, System.Drawing.Color.Empty, System.Drawing.Color.Empty, new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0))));
+            System.Windows.Forms.ListViewItem listViewItem4 = new System.Windows.Forms.ListViewItem(new string[] {
+            ""}, 2, System.Drawing.Color.Empty, System.Drawing.Color.Empty, new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0))));
+            System.Windows.Forms.ListViewItem listViewItem5 = new System.Windows.Forms.ListViewItem(new string[] {
+            ""}, 1, System.Drawing.Color.Empty, System.Drawing.Color.Empty, new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0))));
+            System.Windows.Forms.ListViewItem listViewItem6 = new System.Windows.Forms.ListViewItem(new string[] {
+            ""}, 0, System.Drawing.Color.Empty, System.Drawing.Color.Empty, new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0))));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.calcularButton = new System.Windows.Forms.Button();
+            this.valorCompratextBox = new System.Windows.Forms.TextBox();
+            this.valorPagotextBox = new System.Windows.Forms.TextBox();
+            this.trocoTextBox = new System.Windows.Forms.TextBox();
+            this.moedasImageList = new System.Windows.Forms.ImageList(this.components);
+            this.moedasListView = new System.Windows.Forms.ListView();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBox1.Controls.Add(this.trocoTextBox);
+            this.groupBox1.Controls.Add(this.valorPagotextBox);
+            this.groupBox1.Controls.Add(this.valorCompratextBox);
+            this.groupBox1.Controls.Add(this.calcularButton);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(275, 388);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Valores";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.moedasListView);
+            this.groupBox2.Location = new System.Drawing.Point(293, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(658, 388);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Troco";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Valor da Compra:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Valor Pago:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Valor do Troco:";
+            // 
+            // calcularButton
+            // 
+            this.calcularButton.Location = new System.Drawing.Point(9, 174);
+            this.calcularButton.Name = "calcularButton";
+            this.calcularButton.Size = new System.Drawing.Size(260, 113);
+            this.calcularButton.TabIndex = 1;
+            this.calcularButton.Text = "&CALCULAR";
+            this.calcularButton.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.calcularButton.UseVisualStyleBackColor = true;
+            this.calcularButton.Click += new System.EventHandler(this.calcularButton_Click);
+            // 
+            // valorCompratextBox
+            // 
+            this.valorCompratextBox.Location = new System.Drawing.Point(100, 36);
+            this.valorCompratextBox.Name = "valorCompratextBox";
+            this.valorCompratextBox.Size = new System.Drawing.Size(169, 20);
+            this.valorCompratextBox.TabIndex = 2;
+            // 
+            // valorPagotextBox
+            // 
+            this.valorPagotextBox.Location = new System.Drawing.Point(100, 72);
+            this.valorPagotextBox.Name = "valorPagotextBox";
+            this.valorPagotextBox.Size = new System.Drawing.Size(169, 20);
+            this.valorPagotextBox.TabIndex = 2;
+            // 
+            // trocoTextBox
+            // 
+            this.trocoTextBox.Location = new System.Drawing.Point(100, 108);
+            this.trocoTextBox.Name = "trocoTextBox";
+            this.trocoTextBox.ReadOnly = true;
+            this.trocoTextBox.Size = new System.Drawing.Size(169, 20);
+            this.trocoTextBox.TabIndex = 2;
+            // 
+            // moedasImageList
+            // 
+            this.moedasImageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("moedasImageList.ImageStream")));
+            this.moedasImageList.TransparentColor = System.Drawing.Color.Transparent;
+            this.moedasImageList.Images.SetKeyName(0, "1.png");
+            this.moedasImageList.Images.SetKeyName(1, "5.png");
+            this.moedasImageList.Images.SetKeyName(2, "10.png");
+            this.moedasImageList.Images.SetKeyName(3, "25.png");
+            this.moedasImageList.Images.SetKeyName(4, "50.png");
+            this.moedasImageList.Images.SetKeyName(5, "100.png");
+            // 
+            // moedasListView
+            // 
+            this.moedasListView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.moedasListView.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1,
+            listViewItem2,
+            listViewItem3,
+            listViewItem4,
+            listViewItem5,
+            listViewItem6});
+            this.moedasListView.LargeImageList = this.moedasImageList;
+            this.moedasListView.Location = new System.Drawing.Point(3, 16);
+            this.moedasListView.Name = "moedasListView";
+            this.moedasListView.Size = new System.Drawing.Size(652, 369);
+            this.moedasListView.TabIndex = 0;
+            this.moedasListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // TrocoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(963, 412);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "TrocoForm";
+            this.Text = "Troco";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.TrocoForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button calcularButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox valorCompratextBox;
+        private System.Windows.Forms.TextBox trocoTextBox;
+        private System.Windows.Forms.TextBox valorPagotextBox;
+        private System.Windows.Forms.ImageList moedasImageList;
+        private System.Windows.Forms.ListView moedasListView;
+    }
+}
+
